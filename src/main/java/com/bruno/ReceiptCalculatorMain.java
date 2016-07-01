624da197b9a0d21ad7dc5de46f19e1653620c549
Rocco Bruno <rocco.bruno@digital.hmrc.gov.uk>
2016-07-02 00:01:29 +0100

    Added a simple Main class to test end to end receipt printer

---
+++ src/main/java/com/bruno/ReceiptCalculatorMain.java
@@ -0,0 +1,35 @@
+package com.bruno;
+
+import com.bruno.calculator.SimpleCalculator;
+import com.bruno.model.Basket;
+import com.bruno.service.BasketService;
+import com.bruno.service.BasketServiceImpl;
+import com.bruno.service.FileInputService;
+import com.bruno.service.InputService;
+
+import java.util.Scanner;
+
+public class ReceiptCalculatorMain {
+
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your file name or use default(leave empty): ");
+
+        String fileOption = scanner.next();
+
+        InputService inputService;
+
+        if(fileOption.isEmpty()) {
+            inputService = new FileInputService();
+        } else {
+            inputService = new FileInputService(fileOption);
+        }
+
+        BasketService basketService = new BasketServiceImpl(new SimpleCalculator(),inputService);
+        Basket basket = basketService.loadBasket();
+        System.out.println(basketService.getReceipt(basket));
+
+    }
+
+}
